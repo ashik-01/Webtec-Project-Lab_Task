56e8d30f64eee2b642ac47ddda4872056238f161
S.M. Ashikur Rahman <106684306+ashik-01@users.noreply.github.com>
2022-08-08 23:36:18 +0600

    Add files via upload

---
+++ Lab Task 9/Product.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+
+</head>
+
+    <body class="col-lg-11.5">
+
+<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+
+<h1>ProsGuid Session</h1> 
+<div  style="text-align:right;">
+<ul class="navbar-nav ml-auto ">
+     <h3> .... </h3>
+    <h3 class="nav-item active"><a class="nav-link"href="MainHome.php">Home</a></h3>
+     <h3> .... </h3>
+    <h3 class="nav-item"><a class="nav-link" href="signup.php">Signup</a></h3>
+     <h3> .... </h3>
+    <h3  class="nav-item"><a class="nav-link"  href="UserLogin.php">LogIn</a></h3>
+</ul>
+</div>
+</nav>
+
+
+
+<section class="container">
+        <div class="row row-cols-1 row-cols-md-2 ">
+            <div class="col-lg-4 mr-1 ">
+                <a href="Product.php">
+        <img class="img-fluid w-80"id="101" src="image/car raching.jpg"/> </a>
+            </div>
+
+
+
+                <div class="col-lg-4 m-4 ">
+                <h3>Car Raching Game</h3> <p>4210105108026 _238#2</p>
+                 <h5 >৳5000</h5>
+                 
+
+                        <input type="submit" name="buy"  value="Buy" class="btn btn-info" /><br />                      
+                   
+
+                 </form>
+
+                </div>
+        
+
+</div>
+</section>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ Lab Task 9/showajax.php
@@ -0,0 +1,35 @@
+<?php 
+
+ $conn= mysqli_connect('localhost','root');
+ mysqli_select_db($conn,'final_project');
+
+$sql = "SELECT * FROM orderlist";
+
+    $query = mysqli_query($conn,$sql);
+
+    if(mysqli_num_rows($query)>0){
+        while($result=mysqli_fetch_array($query)){
+
+?>
+<tr>
+
+<td> <?php echo $result['ID']?></td>
+<td> <?php echo $result['Name']?></td>
+<td> <?php echo $result['Address']?></td>
+<td> <?php echo $result['Email']?></td>
+<td> <?php echo $result['PN']?></td>
+<td> <?php echo $result['ProductName']?></td>
+
+
+
+</tr>
+
+<?php
+
+        }
+
+
+    }
+
+?>
+
